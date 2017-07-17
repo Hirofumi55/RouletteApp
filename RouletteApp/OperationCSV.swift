7cacac0a8758bf0050e923dd02aab7963a4edf4f
Hirofumi <sasa1235711@gmail.com>
2017-07-17 13:51:38 +0900

    Error出てるけどとりあえずのCommit

--- RouletteApp/OperationCSV.swift
+++ RouletteApp/OperationCSV.swift
@@ -27,8 +27,29 @@ class OperationCSV {
         //初期値として戻り値に値を格納しておく
         CSVRow = ["0", "名無しさん"]
         
-        
-        //ここに処理を記載する
+        //ここからCSV読み出し処理
+        //CSVファイルのパスを取得する。
+        do {
+            let csvPath = Bundle.main.path(forResource: "mondai", ofType: "csv")
+            
+            //CSVファイルのデータを取得する。
+            let csvData = try String(contentsOfFile:csvPath!, encoding:String.Encoding.utf8)
+            
+            //改行区切りでデータを分割して配列に格納する。
+            var dataList = csvData.componentsSeparatedByString("\n")
+            for var i = 0; i < 6; i += 1 {
+                
+                //カンマ区切りの1行を["aaa", "bbb", ... , "zzz"]形式に変換して代入する
+                let parts = dataList[i].componentsSeparatedByString(",")
+                problemArray.addObject(parts)
+                
+            }
+            var mondai : NSArray = problemArray[counter] as! NSArray
+            
+            
+        }catch _ as NSError{
+            print("error")
+        }
         
         
         return CSVRow
